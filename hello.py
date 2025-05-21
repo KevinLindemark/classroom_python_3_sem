446ad9695f4065e9fae34d155f3945a06face510
KevinLindemark <reclaimyourdata@tutamail.com>
2025-05-21 10:13:09 +0200

    added list test

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
 def hello_world():
+    # Tilpas funktionen så den består testen i hello_test.py
     return "Hello!"
+
+# tilpas funktionen så den returnerer summen af to lister som indeholder intergers og/ eller float værdier
+# Funktionen skal tage to argumenter med lister.
+def list_sum():
+    return
+
--- hello_test.py
+++ hello_test.py
@@ -1,4 +1,10 @@
-import hello;
+import hello
+import re
 
 def test_hello():
-    assert hello.hello_world() == "Hello World!"
+    assert hello.hello_world() == "Hello, worlds!"
+
+def test_lists_sum():
+    l1 = [12, 34, 5435]
+    l2 = [134, 1.444, 44]
+    assert hello.list_sum(l1, l2) == 5660.444
